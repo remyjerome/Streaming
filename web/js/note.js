dd85861cecae1f1234e8fc55c650b86e43025b3d
remyjerome <remy.jerome@live.com>
2015-04-19 15:13:54 +0200

    nothing

--- web/js/note.js
+++ web/js/note.js
@@ -1,39 +1,46 @@
-$('document').ready(function() {
-	$('.note-plus').click(function(){
-		$.ajax({
-			type: 'get',
-			url: Routing.generate('rj_stream_episode_notation:', { saison: {{episode.saison}}, episode: {{episode.episode}},note:1});
-			//url:'http://localhost:8888/Streaming/web/app_dev.php/notation/'+{{episode.saison}}+'/'+{{episode.episode}}+'/1',
-			beforeSend: function() {
-				console.log('Chargement');
-				$('.note-plus').addClass('progress-bar-striped active');
-			},
-			success: function(data) {
-				console.log(data.note);
-				$('.note-plus').css('width',data.note+'%');
-				$('.note-moins').css('width',100 - data.note+'%');
-				$('.note-plus').removeClass('progress-bar-striped active');
+    $('document').ready(function() {
+        var episode = {{ episode.episode|json_encode() }};
+        var saison = {{ episode.saison|json_encode() }};
+        $('.note-plus').click(function(){
+            $.ajax({
+                type: 'get',
+                
+                url: Routing.generate('rj_stream_episode_notation', { saison: saison, episode: episode, note:1}),
+                beforeSend: function() {
+                    console.log('Chargement');
+                    $('.note-plus').addClass('progress-bar-striped active');
+                },
+                success: function(data) {
+                    console.log(data.note);   
+                    if(data.note != "")
+                    {        
+                        $('.note-plus').css('width',data.note+'%');
+                        $('.note-moins').css('width',100 - data.note+'%');
+                        $('.note-plus').attr('data-original-title', data.nbplus+' votes');
+                    }
+                    $('.note-plus').removeClass('progress-bar-striped active');
 
-			}
-		});
-	});
-	$('.note-moins').click(function(){
-		$.ajax({
-			type: 'get',
-			url:'http://localhost:8888/Streaming/web/app_dev.php/notation/2/1/0',
-			beforeSend: function() {
-				console.log('Chargement');
-				$('.note-moins').addClass('progress-bar-striped active');
-			},
-			success: function(data) {
-				console.log(data.note);
-				$('.note-plus').css('width',data.note+'%');
-				$('.note-moins').css('width',100 - data.note+'%');
-				$('.note-moins').removeClass('progress-bar-striped active');
-			}
-		});
-	});
-});
-
-//url:'http://localhost:8888/Streaming/web/app_dev.php/notation/'+{{episode.saison}}+'/'+{{episode.episode}}+'/1',
-//url:'http://localhost:8888/Streaming/web/app_dev.php/notation/'+{{episode.saison}}+'/'+{{episode.episode}}+'/0',
+                }
+            });
+        });
+        $('.note-moins').click(function(){
+            $.ajax({
+                type: 'get',
+                url: Routing.generate('rj_stream_episode_notation', { saison: saison, episode: episode, note:0}),
+                beforeSend: function() {
+                    console.log('Chargement');
+                    $('.note-moins').addClass('progress-bar-striped active');
+                },
+                success: function(data) {
+                    console.log(data.note);
+                    if(data.note != "")
+                    {
+                        $('.note-plus').css('width',data.note+'%');
+                        $('.note-moins').css('width',100 - data.note+'%');
+                        $('.note-moins').attr('data-original-title', data.nbmoins+' votes');
+                    }
+                    $('.note-moins').removeClass('progress-bar-striped active');
+                }
+            });
+        });
+    });
